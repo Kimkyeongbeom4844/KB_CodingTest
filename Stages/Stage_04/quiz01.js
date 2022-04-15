76b6c0a8803a33fbae815f96d8cce87c9852461e
Kimkyeongbeom4844 <bibix321@naver.com>
2022-04-16 01:05:28 +0900

    04_quiz01은 다시한번 봐볼것!
    대박 신기함..

--- Stages/Stage_04/quiz01.js
+++ Stages/Stage_04/quiz01.js
@@ -2,20 +2,20 @@ let fs = require('fs');
 let input = fs.readFileSync('../sample.txt').toString().split('\n');
 let count = Number(input[0]);
 let compare = input[1].split(" ");
-let max = Number(compare[0]);
-let min = Number(compare[0]);
+// let max = Number(compare[0]);
+// let min = Number(compare[0]);
 let arrays = [];
 for(let i=0; i<count; i++){
-    if(Number(compare[i])>max){
-        max = Number(compare[i]);
-    }
-    else if(Number(compare[i])<min){
-        min = Number(compare[i]);
-    }
-    // arrays[i] = Number(compare[i]);
-    // max = Math.max(...arrays);
-    // min = Math.min(...arrays);
+    // if(Number(compare[i])>max){
+    //     max = Number(compare[i]);
+    // }
+    // else if(Number(compare[i])<min){
+    //     min = Number(compare[i]);
+    // }
+    arrays[i] = Number(compare[i]);
 }
+let max = Math.max(...arrays);
+let min = Math.min(...arrays);
 console.log(`${min} ${max}`);
 // const fs = require('fs');
 // const input = fs.readFileSync('../sample.txt').toString().trim().split('\n');
